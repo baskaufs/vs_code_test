cf526238be1d97718a692f8a3fb129f35b783291
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2023-01-31 07:32:07 -0600

    more testing

--- wikidata_data.py
+++ wikidata_data.py
@@ -1,22 +1,28 @@
 import requests
 import json
 
+USER_AGENT = 'Wikidata-Data-Extraction/0.1 (steve.baskauf@vanderbilt.edu)'
+HEADERS = {'User-Agent': USER_AGENT, 'Accept': 'application/sparql-results+json'}
+
+
 def get_data_from_wikidata_api(query):
     url = 'https://query.wikidata.org/sparql'
-    r = requests.get(url, params={'format': 'json', 'query': query})
-    data = r.json()
+    HEADERS['query'] = query
+    r = requests.post(url, headers=HEADERS, data={'query': query})
+    #data = r.json()
+    data = r.text
     return data
 
-query = '''
-SELECT ?item ?itemLabel ?description
+query = '''SELECT ?item ?itemLabel ?description
 WHERE
 {
-    ?item wdt:P31 wd:Q5.
-    ?item dcterms:description ?description.
+    ?item wdt:P195 wd:Q18563658.
+    ?item schema:description ?description.
     FILTER (lang(?description) = "en")
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". 
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
     }
     '''
 
 results = get_data_from_wikidata_api(query)
-print(json.dumps(results, indent=4, sort_keys=True))
+print(results)
+#print(json.dumps(results, indent=4, sort_keys=True))
